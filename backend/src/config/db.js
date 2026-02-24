f0c88dd0c7974533b87e40a493dddc1e4cd98866
dhanusri-t <tdhanusri758@gmail.com>
2026-02-24 13:24:43 +0530

    hided secrets

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql2/promise";
 export const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "sri@1306",
+  password: process.env.DB_PW ,
   database: "workflow_monitoring",
 });
